d3f55dce709bb3880f5102cd2f18cae50ef93a5d
acontrerasregueiro <acontrerasregueiro@gmail.com>
2022-04-18 17:20:09 +0000

    Exportando e importando modulo externo Contador

---
+++ src/js/component/contador.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+/*
+const Contador = () => {
+	return <div>loladasdasdasdlalo</div>;
+};
+
+export default Contador;
+*/
+
+export const Contador = () => {
+	return <div>loladasdasdasdlalo</div>;
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Contador } from "./component/contador.jsx";
+//import Y, { ExFunc1, ExFunc2, etc... } from 'module';
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
-
+//import Contador from "./component/contador.jsx";
+//import Home from "./component/home.jsx";
+//ReactDOM.render(<Home />, document.querySelector("#app"));
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Contador />, document.querySelector("#app"));
